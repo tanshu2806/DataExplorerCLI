a6c9ec0dc514cd067aa02ba3770c2a7252d725e9
tanshu2806 <tanshutanishk2806@gmail.com>
2026-08-05 02:13:57 +0530

    all function working

--- explorer.py
+++ explorer.py
@@ -1,5 +1,6 @@
-from numpy import dtype
 import pandas as pd
+
+
 class DataExplorer:
 
     def __init__(self):
@@ -17,21 +18,111 @@ class DataExplorer:
         else:
             print("CSV loaded successfully")
             rows, columns = self.df.shape
-            print("Rows : ",rows)
-            print("Columns : ",columns)   
+            print("Rows : ", rows)
+            print("Columns : ", columns)
+
+    def is_loaded(self):
+        if self.df is None:
+            print("Please load a CSV file first.")
+            return False
+
+        return True
 
     def dataset_info(self):
-        if self.df.empty:
-            print("Please upload the CSV first.")
-        else:
-            rows, column = self.df.shape
-            print("Rows: ", rows)     
-            print("Columns: ", column)
-            print("Datatype: ",dtype)
-            print("Memory Usage: ", self.df.info())
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+        rows, columns = self.df.shape
+        print("Rows: ", rows)
+        print("Columns: ", columns)
+        print("Datatype: ", self.df.dtypes)
+        self.df.info()
 
-    def head(self):
-        print(self.df.head())
+    def show_shape(self):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
 
-    def tail(self):
-        print(self.df.tail())     
+        rows, columns = self.df.shape
+        print(f"The dataset has {rows} rows and {columns} columns.")
+        pass
+
+    def show_columns(self):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        print(self.df.columns.to_list())
+
+    def show_dtypes(self):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        print("Column Datatypes: ")
+        print(self.df.dtypes)
+        pass
+
+    def head(self, rows=5):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        print(self.df.head(rows))
+
+    def tail(self, rows=5):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        print(self.df.tail(rows))
+
+    def missing_values(self):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        print("Number of Missing Values:")
+        print(self.df.isnull().sum())
+        pass
+
+    def statistics(self):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        print("Statistical summary:")
+        print(self.df.describe(include='all'))
+
+    def correlation_matrix(self):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        print("Correlation matrix: ")
+        numeric_df = self.df.select_dtypes(include=['number'])
+        print(numeric_df.corr())
+
+    def remove_missing_values(self):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        rows, columns = self.df.shape
+        print("Original Rows: ", rows)
+
+        cleaned = self.df.dropna()
+
+        new_rows, columns = cleaned.shape
+        print("After Removal Rows: ", new_rows)
+        print(f"{rows-new_rows} rows removed")
+
+    def export_csv(self,name):
+        if not self.is_loaded():
+            return
+        assert self.df is not None
+
+        path = "exports/" + name
+        self.df.to_csv(path,index= False)
+        print(f"""CSV exported successfully!
+                Location: {path}""")
--- main.py
+++ main.py
@@ -2,30 +2,103 @@ from explorer import DataExplorer
 
 explorer = DataExplorer()
 
+def get_row_count():
+    while True:
+        try:
+            value = input("How many rows? (Press Enter for default)").strip()
+            if value =="":
+                return 5
+            rows = int(value)
+            
+            if rows <= 0:
+                print("Please enter a number greater than 0.")
+                continue
+            else:
+                return rows
+        except ValueError:
+            print("Please enter a valid number.")
+            
+        
+
 while True:
-    print('''========== DATA EXPLORER ==========
+    print('''========== DATA EXPLORER CLI ==========
 
     1. Load CSV
-    2. Dataset Info
-    3. Top Rows
-    4. Bottom Rows
-    5. Exit
+    2. Dataset Information
+    3. Show Dataset Shape
+    4. Show Column Names
+    5. Show Data Types
+    6. Show Top Rows
+    7. Show Bottom Rows
+    8. Missing Values
+    9. Statistical Summary
+    10. Correlation Matrix
+    11. Remove Missing Values
+    12. Export Cleaned CSV
+
+    0. Exit
+
 
     Enter choice:''')
-
-    choice = int(input("Enter the choice: "))
+    try:
+        choice = int(input("Enter the choice: "))
+    except ValueError:
+        print("Please enter a valid number.")
 
     match choice:
         case 1:
+            # Load CSV
             path = input("Enter file path: ")
             explorer.load_csv(path)
+
         case 2:
+            # Dataset Information
             explorer.dataset_info()
+
         case 3:
-            explorer.head()
+            # Dataset Shape
+            explorer.show_shape()
+
         case 4:
-            explorer.tail()
+            # Column Names
+            explorer.show_columns()
+
         case 5:
-            break
+            # Data Types
+            explorer.show_dtypes()
+
+        case 6:
+            # Top rows
+            rows = get_row_count()
+            explorer.head(rows)
+
+        case 7:
+            # Bottom Rows
+            rows = get_row_count()
+            explorer.tail(rows)
+
+        case 8:
+            # Missing Values
+            explorer.missing_values()
+
+        case 9:
+            # Statistical Summary
+            explorer.statistics()
+
+        case 10:
+            # Correlation Matrix
+            explorer.correlation_matrix()
+
+        case 11:
+            # Remove Missing Values
+            explorer.remove_missing_values()
+
+        case 12:
+            # Export CSV
+            name = input("Enter filename: ")
+            if not name.endswith(".csv"):
+                name = name + ".csv"
+            explorer.export_csv(name)
+
         case _:
             print("Invalid Choice")
